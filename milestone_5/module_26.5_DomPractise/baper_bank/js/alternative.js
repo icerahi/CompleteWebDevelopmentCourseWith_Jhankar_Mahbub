bfc2df86439ce13c6d435b22d2e2dd6f5073dd31
rahidarf <rootcodeerror@gmail.com>
2021-08-17 14:07:39 +0600

    alternative functional baper bank

---
+++ milestone_5/module_26.5_DomPractise/baper_bank/js/alternative.js
@@ -0,0 +1,37 @@
+document.getElementById("deposit_btn").addEventListener("click", function () {
+  ChangeBalance("deposit");
+});
+document.getElementById("withdraw_btn").addEventListener("click", function () {
+  ChangeBalance("withdraw");
+});
+
+function ChangeBalance(type) {
+  //get input value
+  const newAmount = document.getElementById(`${type}_input`);
+  const amount = parseFloat(newAmount.value);
+  newAmount.value = "";
+
+  if (amount > 0) {
+    // type of total
+    const getTotal = document.getElementById(`total_${type}`);
+    const total = parseFloat(getTotal.innerText);
+
+    // update balance
+    const getBalance = document.getElementById("total_balance");
+    const balance = parseFloat(getBalance.innerText);
+
+    if (type == "withdraw") {
+      if (balance > amount) {
+        getTotal.innerText = total + amount;
+        getBalance.innerText = balance - amount;
+      } else {
+        alert("Your balance is lower than your withdraw amount!!");
+      }
+    } else {
+        getTotal.innerText = total + amount;
+      getBalance.innerText = balance + amount;
+    }
+  } else {
+    alert("Amount must be a positive number");
+  }
+}
